457a4033ddc98f4a364cccd3907ac8488694a953
sriramez <velayutham.sriram@gmail.com>
2023-01-02 14:55:19 -0500

    added get all patrols controller

--- src/main/java/com/speedlog/controller/PoliceStationController.java
+++ src/main/java/com/speedlog/controller/PoliceStationController.java
@@ -49,6 +49,7 @@ public class PoliceStationController {
 		return policeStationService.getPoliceStationWithName(policeStationName);
 	}
 	
+	@GetMapping("patrols")
 	List<PoliceStationToRetModel> getAllPoliceStation()
 	{
 		return policeStationService.getAllPoliceStation();
